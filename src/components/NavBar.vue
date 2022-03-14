3cd82c49b93b626a2c4ffb9a465bacc34f0fe559
Manel-Gonzalez <manel.gonzalez.rico@gmail.com>
2022-03-14 16:17:58 +0100

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>  
+        <h1>ZAWARUDO</h1>
+        <button @click="sayHello">PRESIONAME</button> 
+    </div>
+</template>
+
+<script>
+export default {
+    name:"NavBar",
+
+    methods:{
+        sayHello(){
+            console.log("IT WAS ME DIO")
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
